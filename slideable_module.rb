10da55e3c6bbd290c0859758827a20fa2f907a10
matt <sidellmatt@gmail.com>
2021-03-23 18:40:09 -0400

    Slideable

--- slideable_module.rb
+++ slideable_module.rb
@@ -6,7 +6,13 @@ module Slideable
             x, y = direction[0], direction[1]
             new_x = self.pos[0] + x
             new_y = self.pos[1] + y
-            moves_arr << [new_x, new_y]
+            blocked = false
+            until blocked
+                blocked = true if self.board[new_x][new_y].color != self.color
+                moves_arr << [new_x, new_y] unless blocked
+                new_x += x
+                new_y += y
+            end
         end
         moves_arr
     end
