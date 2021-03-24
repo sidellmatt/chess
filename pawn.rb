ea0c526dbd1363f9b296c8c4f6741f75c6697f7b
matt <sidellmatt@gmail.com>
2021-03-24 18:29:31 -0400

    Fixed side attacks

--- pawn.rb
+++ pawn.rb
@@ -42,17 +42,17 @@ class Pawn < Piece
         attacks = []
         if self.color == "white"
             if self.board.rows[self.pos[0] + self.forward_dir][self.pos[1] - 1].color == "black"
-                attacks << [self.pos[0] + self.forward_dir][self.pos[1] - 1]
+                attacks << [(self.pos[0] + self.forward_dir), (self.pos[1] - 1)]
             end
             if self.board.rows[self.pos[0] + self.forward_dir][self.pos[1] + 1].color == "black"
-                 attacks << [self.pos[0] + self.forward_dir][self.pos[1] + 1]
+                 attacks << [(self.pos[0] + self.forward_dir), (self.pos[1] + 1)]
             end
         elsif self.color == "black"
             if self.board.rows[self.pos[0] + self.forward_dir][self.pos[1] - 1].color == "white"
-                attacks << [self.pos[0] + self.forward_dir][self.pos[1] - 1]
+                attacks << [(self.pos[0] + self.forward_dir), (self.pos[1] - 1)]
             end
             if self.board.rows[self.pos[0] + self.forward_dir][self.pos[1] + 1].color == "white"
-                 attacks << [self.pos[0] + self.forward_dir][self.pos[1] + 1]
+                 attacks << [(self.pos[0] + self.forward_dir), (self.pos[1] + 1)]
             end
         end
         attacks
