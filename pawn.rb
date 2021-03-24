e982d8447655be1fc42670404f6827e177b77819
matt <sidellmatt@gmail.com>
2021-03-23 22:44:20 -0400

    Pawn again

--- pawn.rb
+++ pawn.rb
@@ -27,7 +27,11 @@ class Pawn < Piece
 
     def forward_steps
         steps = []
-        steps << [self.pos[0] + self.forward_dir, self.pos[1]] if self.board.valid_pos([[self.pos[0] + self.forward_dir, self.pos[1]]]) && self.board.rows[self.pos[0] + self.forward_dir][self.pos[1]].color == nil
+        if self.board.valid_pos?([[self.pos[0] + self.forward_dir, self.pos[1]]])
+            if self.board.rows[self.pos[0] + self.forward_dir][self.pos[1]].color == nil
+                steps << [self.pos[0] + self.forward_dir, self.pos[1]]
+            end
+        end
         if self.at_start_row?
             steps << [self.pos[0] + (self.forward_dir * 2), self.pos[1]] if self.board.rows[self.pos[0] + (self.forward_dir * 2)][self.pos[1]].color == nil
         end
